7192c48128018ff7831c631cf686d3e5710064d9
Louise Thomsen <loui718d@stud.kea.dk>
2022-09-28 10:46:07 +0200

    nyhedsbrev

--- src/main.js
+++ src/main.js
@@ -5,51 +5,34 @@ const acceptCookie = document.querySelector("#accepter");
 const denyCookie = document.querySelector("#afvis");
 const cookieboks = document.querySelector("#cookieboks");
 const popUp = document.querySelector("#nyhedsbrev");
-const tilmeldt = document.querySelector("#tilmeld");
+
+const tilmeldt = document.querySelector("#fjern");
 
 acceptCookie.addEventListener("click", fjernCookieBoks);
 denyCookie.addEventListener("click", fjernCookieBoks);
 
 function fjernCookieBoks() {
-  cookieboks.classList.add("hidden");
-  sessionStorage.setItem("fjerncookieboks", true);
+  cookieboks.classList.add("hid");
 }
 
-if (sessionStorage.setItem("fjerncookieboks")) {
-  cookieboks.classList.add("hidden");
-}
+// sessionStorage.setItem("fjernCookieboks", true);
+// if (sessionStorage.setItem("fjernCookieboks")) {
+//   cookieboks.classList.add("hid");
+// }
 
 // nyhedsbrev
+
+const myTimeout = setTimeout(visNyhedsbrev, 5000);
+
+function visNyhedsbrev() {
+  popUp.style.display = "block";
+  document.getElementById("blur").style.display = "block";
+}
 
 tilmeldt.addEventListener("click", fjernNyhedsbrev);
 
 function fjernNyhedsbrev() {
   console.log("clickknap");
-  popUp.classList.remove("scale-in-center");
-  popUp.classList.add("hidden");
-  sessionStorage.setItem("fjernNyhedsbrev", true);
+  popUp.style.display = "none";
+  document.getElementById("blur").style.display = "none";
 }
-
-if (sessionStorage.setItem("fjernNyhedsbrev")) {
-  popUp.classList.add("hidden");
-}
-
-// const nyhedsbrev = document.querySelector("#hid");
-// window.onload = function () {
-//   setTimeout(popUp, 4000);
-// };
-
-// function popUp() {
-//   console.log("Hello world");
-//   nyhedsbrev.style.display = "block";
-// }
-
-window.addEventListener("load", function () {
-  setTimeout(function open(event) {
-    document.querySelector(".nyhedsbrev").style.display = "block";
-  }, 2000);
-});
-
-document.querySelector("#tilmeld").addEventListener("click", function () {
-  document.querySelector(".nyhedsbrev").style.display = "none";
-});
